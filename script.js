cac22655a2c70c6a6c5b27ccecc99dafcb333754
Matthys <33497379+Taese@users.noreply.github.com>
2018-12-22 12:08:46 +0100

    1

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleSidebar(){
+	document.getElementById('Sidebar').classList.toggle('active');
+	document.getElementById('Btn').classList.toggle('active');
+	document.getElementById('Main').classList.toggle('active');
+} 
