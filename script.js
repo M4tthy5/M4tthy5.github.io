e49933c6fb5c23d4f0968d42e288e9e27a5bd44b
Matthys <33497379+Taese@users.noreply.github.com>
2018-12-29 22:34:32 +0100

    8

--- script.js
+++
@@ -1,5 +0,0 @@
-function toggleSidebar(){
-	document.getElementById('Sidebar').classList.toggle('active');
-	document.getElementById('Btn').classList.toggle('active');
-	document.getElementById('Main').classList.toggle('active');
-} 
